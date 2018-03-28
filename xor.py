63b5be805dc56631ffadf5d7475ab6e18ddcac60
Baraa Said <baraasaid@think-it.io>
2018-03-28 19:18:14 +0100

    renaming functions and improving file encoding functions

--- xor.py
+++ xor.py
@@ -5,38 +5,31 @@ import sys
 import glob
 from Crypto.Cipher import XOR
 
-def cipherb(key, plaintext):
+def XorCipher(key, plaintext):
     cipher_obj = XOR.new(key)
     cipher_obj = cipher_obj.encrypt(plaintext)
     return cipher_obj
 
-def file_encoding1(path, key,  destination_path):
-    
+def FileEncoding(path, key,  destination_path = None):
+
     with open(path, mode='rb') as i_file:
         plaintext = i_file.read()
-    
-    with open(destination_path, mode='xb') as o_file:
-        o_file.write(cipherb(key, plaintext))
-        
+    if destination_path == None :
+        return XorCipher(key, plaintext)
+    else :
+        with open(destination_path, mode='xb') as o_file:
+            o_file.write(XorCipher(key, plaintext))
 
 
 
-def file_encoding2(path, key):
-    
-    with open(path, mode='rb') as i_file:
-        plaintext = i_file.read()
-    return cipherb(key, plaintext)
-
-
-
-def isDirectory(d):
+def IsDirectory(d):
     
     if os.path.isdir(d) :
         return True
     else:
         return False
 
-def isDirectoryExist(d):
+def IsDirectoryExist(d):
     if os.path.exists(d):
         return True
     else:
@@ -52,10 +45,10 @@ def main():
         path = sys.argv[1]
         cipher = sys.argv[2]
 
-        if not isDirectoryExist(path):
+        if not IsDirectoryExist(path):
             sys.exit('Inexistant input')
-        elif not isDirectory(path):
-            sys.stdout.buffer.write(file_encoding2(path, cipher))
+        elif not IsDirectory(path):
+            sys.stdout.buffer.write(FileEncoding(path, cipher, None))
         else:
             sys.exit('Missing arguments, required 3 at least')
     elif arguments == 4:
@@ -63,10 +56,10 @@ def main():
         cipher = sys.argv[2]
         rootDest = sys.argv[3]
 
-        if not isDirectoryExist(rootDir):
+        if not IsDirectoryExist(rootDir):
             sys.exit('Inexistant input')
-        elif not isDirectory(rootDir):
-            file_encoding1(rootDir, cipher, rootDest)
+        elif not IsDirectory(rootDir):
+            FileEncoding(rootDir, cipher, rootDest)
             
         else:
             for dirName, subdirList, fileList in os.walk(rootDir):
@@ -83,7 +76,7 @@ def main():
                     for fname in fileList:
                         ifname = os.path.join(dirName, fname)
                         ofname = os.path.join(destDir, fname)
-                        file_encoding1(ifname, cipher,ofname)
+                        FileEncoding(ifname, cipher,ofname)
 if __name__ == '__main__':
     main()
 
